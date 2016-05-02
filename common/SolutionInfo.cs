f1112d28c83b9f9694f092b52df8bc7ebccf385b
Justin Rusbatch <jrusbatch@gmail.com>
2016-05-02 14:57:08 -0400

    Add a build script

---
+++ common/SolutionInfo.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("RimDev.Permissions")]
+[assembly: AssemblyCompany("Ritter Insurance Marketing, LLC")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
